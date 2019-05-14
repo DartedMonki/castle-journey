7f2cf67ff0af6d9c8bebf4e742cadb32a1272a30
reyhannuurakbar <reyhannuurakbar@gmail.com>
2019-05-15 00:36:27 +0700

    nambah asset, progress pausemenu

---
+++ Castle Journey/Assets/Scripts/UI/PauseScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PauseScript : MonoBehaviour
+{
+
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    public AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = FindObjectOfType<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    public void Resume()
+    {
+        audioSource.Play(0);
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause ()
+    {
+        audioSource.Pause();
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+}
