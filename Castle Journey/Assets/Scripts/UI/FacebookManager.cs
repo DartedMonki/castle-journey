eabb22c403851ae0088250ccf11381001e4db37c
reyhannuurakbar <reyhannuurakbar@gmail.com>
2019-05-25 12:24:10 +0700

    testing end game yang konek ke facebook

---
+++ Castle Journey/Assets/Scripts/UI/FacebookManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Facebook.Unity;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class FacebookManager : MonoBehaviour
+{
+    public Text userIdText;
+
+    private void Awake()
+    {
+        if (!FB.IsInitialized)
+        {
+            FB.Init();
+        }
+        else
+        {
+            FB.ActivateApp();
+        }
+    }
+
+    public void LogIn()
+    {
+        FB.LogInWithReadPermissions(callback:OnLogIn);
+    }
+
+    private void OnLogIn(ILoginResult result)
+    {
+        if (FB.IsLoggedIn)
+        {
+            AccessToken Token = AccessToken.CurrentAccessToken;
+            userIdText.text = Token.UserId;
+        }
+        else
+            Debug.Log("Canceled Login");
+    }
+}
